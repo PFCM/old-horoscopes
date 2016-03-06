7ec59219d191c98c0c7594b11b2a7462c76b3f69
pfcm <pfcmathews@gmail.com>
2016-03-07 10:29:53 +1300

    add endpoint stubs for getting horoscope and turning a date of birth into a star sign

--- app/handlers.py
+++ app/handlers.py
@@ -2,7 +2,7 @@
 This module defines handlers/view functions for actually serving web pages.
 """
 
-from flask import render_template
+from flask import render_template, url_for
 from flask import Flask
 
 APP = Flask(__name__)
@@ -13,7 +13,32 @@ def hello():
     """
     Serves the front page.
     """
-    return render_template('hello.html')
+    return render_template('hello.html',
+                           horoscope_url=url_for('/_horoscope.json'),
+                           starsign_url=url_for('/_starsign.json'))
+
+
+@APP.route('/_horoscope.json')
+def get_horoscope():
+    """
+    Fetches a horoscope from someone else and returns it.
+
+    Parameters:
+        sign (str): the star sign the horoscope is for.
+        date (YYYY-MM-DD): the date to find a horoscope for.
+    """
+    pass
+
+
+@APP.route('/_starsign.json')
+def get_starsign():
+    """
+    Gets someone's star sign.
+
+    Parameters:
+        date (YYYY-MM-DD): the date of the person's birth.
+    """
+    pass
 
 
 if __name__ == '__main__':
