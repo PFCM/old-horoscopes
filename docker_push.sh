473c043aff2f13baa3d25b2360200db321da3655
pfcm <pfcmathews@gmail.com>
2016-03-07 16:27:24 +1300

    fingers crossed

--- docker_push.sh
+++ docker_push.sh
@@ -4,4 +4,4 @@
 # builds the image, pushes to docker hub
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker build -t pfcm/old-horoscopes .
-docker push
+docker push pfcm/old-horoscopes:latest
