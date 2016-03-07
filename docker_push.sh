7ba1bed3b75aa0ad21bc5402c519a904cd8a9e80
pfcm <pfcmathews@gmail.com>
2016-03-07 17:05:59 +1300

    force override of latest tag

--- docker_push.sh
+++ docker_push.sh
@@ -4,6 +4,6 @@ set -e
 # builds the image, pushes to docker hub
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker build -t pfcm/old-horoscopes:$COMMIT .
-docker tag pfcm/old-horoscopes:$COMMIT pfcm/old-horoscopes:latest
+docker tag -f pfcm/old-horoscopes:$COMMIT pfcm/old-horoscopes:latest
 docker tag pfcm/old-horoscopes:$COMMIT pfcm/old-horoscopes:build-$TRAVIS_BUILD_NUMBER
 docker push pfcm/old-horoscopes
