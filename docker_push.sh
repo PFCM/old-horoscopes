7b589885196ab5a213bdfba51ade72f00f79a78d
pfcm <pfcmathews@gmail.com>
2016-03-07 16:43:16 +1300

    more tags

--- docker_push.sh
+++ docker_push.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
-
+set -e
 # deployment script
 # builds the image, pushes to docker hub
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker build -t pfcm/old-horoscopes:$COMMIT .
 docker tag pfcm/old-horoscopes:$COMMIT pfcm/old-horoscopes:latest
+docker tag pfcm/old-horoscopes:$COMMIT pfcm/old-horoscopes:build-$TRAVIS_BUILD_NUMBER
 docker push pfcm/old-horoscopes
