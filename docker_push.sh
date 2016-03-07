37424c3745013b99121bd3a3ba8c2686f03d979e
pfcm <pfcmathews@gmail.com>
2016-03-07 16:01:42 +1300

    add deploy script

---
+++ docker_push.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# deployment script
+# builds the image, pushes to docker hub
+docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+docker build -t pfcm/old-horoscopes .
+docker push
