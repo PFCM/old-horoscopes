aa3dc531d88a95d54fdc8fa532f271f41f4c1c41
pfcm <pfcmathews@gmail.com>
2016-03-07 16:39:04 +1300

    slightly update deploy script

--- docker_push.sh
+++ docker_push.sh
@@ -3,5 +3,6 @@
 # deployment script
 # builds the image, pushes to docker hub
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker build -t pfcm/old-horoscopes .
-docker push pfcm/old-horoscopes:latest
+docker build -t pfcm/old-horoscopes:$COMMIT .
+docker tag pfcm/old-horoscopes:$COMMIT pfcm/old-horoscopes:latest
+docker push pfcm/old-horoscopes
