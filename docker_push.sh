760926d4bc46ffbe02d8c659422e63262e0a2c2e
pfcm <pfcmathews@gmail.com>
2016-03-07 16:47:06 +1300

    fix quoting of password

--- docker_push.sh
+++ docker_push.sh
@@ -2,7 +2,7 @@
 set -e
 # deployment script
 # builds the image, pushes to docker hub
-docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p=$DOCKER_PASSWORD
 docker build -t pfcm/old-horoscopes:$COMMIT .
 docker tag pfcm/old-horoscopes:$COMMIT pfcm/old-horoscopes:latest
 docker tag pfcm/old-horoscopes:$COMMIT pfcm/old-horoscopes:build-$TRAVIS_BUILD_NUMBER
